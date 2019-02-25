6eeee29cdfa107c51cf00a7206ef80d861722f7a
Joseph Cochran <jmc361@zips.uakron.edu>
2019-02-25 10:14:40 -0600

    Add files via upload

---
+++ frantic_crift_curcan1.ino
@@ -0,0 +1,73 @@
+/*
+  Ping))) Sensor
+
+  This sketch reads a PING))) ultrasonic
+  rangefinder and returns the distance to the
+  closest object in range. To do this, it sends a
+  pulse to the sensor to initiate a reading, then
+  listens for a pulse to return.  The length of
+  the returning pulse is proportional to the
+  distance of the object from the sensor.
+
+  The circuit:
+   * +V connection of the PING))) attached to +5V
+   * GND connection attached to ground
+   * SIG connection attached to digital pin 7
+
+  http://www.arduino.cc/en/Tutorial/Ping
+
+  This example code is in the public domain.
+*/
+#include <Servo.h>
+
+int pos = 0;
+
+Servo servo_9;
+
+int inches = 0;
+
+int cm = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  servo_9.attach(9);
+
+}
+
+void loop()
+{
+  // measure the ping time in cm
+  cm = 0.01723 * readUltrasonicDistance(7, 7);
+  // convert to inches by dividing by 2.54
+  inches = (cm / 2.54);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.println("cm");
+  
+  if(cm <= 25)
+  {
+    servo_9.write(180);
+  }
+  if(cm >= 26)
+  {
+    servo_9.write(0);
+  }
+  
+  delay(100); // Wait for 100 millisecond(s)
+}
